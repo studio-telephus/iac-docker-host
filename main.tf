df8de578575909c0c14f59e3543d01421d2b0b5e
Kristo Aun <kristoaun@live.com>
2024-01-22 14:11:04 +0200

    fmt

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "docker_network" "dev_network" {
-  name = "dev-network"
+  name       = "dev-network"
   attachable = true
-  ipv6   = false
-  driver = "bridge"
+  ipv6       = false
+  driver     = "bridge"
   ipam_config {
     ip_range = "10.20.0.0/24"
     subnet   = "10.20.0.0/24"
