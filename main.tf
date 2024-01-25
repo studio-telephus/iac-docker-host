fa8aa0f7a7058b601b9842f1275da5d8f77b646e
Kristo Aun <kristoaun@live.com>
2024-01-25 16:25:08 +0200

    host

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 resource "docker_network" "network_dev_docker" {
-  name       = "dev-docker"
-  attachable = true
-  ipv6       = false
+  name            = "dev-docker"
+  attachable      = true
+  ipv6            = false
   check_duplicate = true
-  driver     = "bridge"
+  driver          = "bridge"
   ipam_config {
-    ip_range = "10.30.0.0/24"
-    subnet   = "10.30.0.0/24"
-    gateway  = "10.30.0.1"
+    ip_range = "10.20.0.0/24"
+    subnet   = "10.20.0.0/24"
+    gateway  = "10.20.0.1"
   }
   options = {
-    "com.docker.network.bridge.name" = "dev-docker"
+    "com.docker.network.bridge.name"                 = "dev-docker"
     "com.docker.network.bridge.host_binding_ipv4"    = "0.0.0.0"
     "com.docker.network.driver.mtu"                  = "1500"
     "com.docker.network.bridge.enable_ip_masquerade" = "true"
@@ -19,18 +19,18 @@ resource "docker_network" "network_dev_docker" {
 }
 
 resource "docker_network" "network_tst_docker" {
-  name       = "tst-docker"
-  attachable = true
-  ipv6       = false
+  name            = "tst-docker"
+  attachable      = true
+  ipv6            = false
   check_duplicate = true
-  driver     = "bridge"
+  driver          = "bridge"
   ipam_config {
     ip_range = "10.30.0.0/24"
     subnet   = "10.30.0.0/24"
     gateway  = "10.30.0.1"
   }
   options = {
-    "com.docker.network.bridge.name" = "tst-docker"
+    "com.docker.network.bridge.name"                 = "tst-docker"
     "com.docker.network.bridge.host_binding_ipv4"    = "0.0.0.0"
     "com.docker.network.driver.mtu"                  = "1500"
     "com.docker.network.bridge.enable_ip_masquerade" = "true"
