7c3c4b731a05bd4eecd7f30b49c8008f0e11af52
Kristo Aun <kristoaun@live.com>
2024-01-25 15:07:15 +0200

    host

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "docker_network" "network_dev_docker" {
   name       = "network-dev-docker"
   attachable = true
   ipv6       = false
-  driver     = "bridge"
+  driver     = "host"
   ipam_config {
     ip_range = "10.20.0.0/24"
     subnet   = "10.20.0.0/24"
