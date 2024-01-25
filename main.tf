fd9e8706330bddce90aaf5bc29435d1010f77a80
Kristo Aun <kristoaun@live.com>
2024-01-25 16:22:45 +0200

    host

--- main.tf
+++ main.tf
@@ -2,14 +2,19 @@ resource "docker_network" "network_dev_docker" {
   name       = "dev-docker"
   attachable = true
   ipv6       = false
+  check_duplicate = true
   driver     = "bridge"
   ipam_config {
-    ip_range = "10.20.0.0/24"
-    subnet   = "10.20.0.0/24"
-    gateway  = "10.20.0.1"
+    ip_range = "10.30.0.0/24"
+    subnet   = "10.30.0.0/24"
+    gateway  = "10.30.0.1"
   }
   options = {
     "com.docker.network.bridge.name" = "dev-docker"
+    "com.docker.network.bridge.host_binding_ipv4"    = "0.0.0.0"
+    "com.docker.network.driver.mtu"                  = "1500"
+    "com.docker.network.bridge.enable_ip_masquerade" = "true"
+    "com.docker.network.bridge.enable_icc"           = "true"
   }
 }
 
@@ -17,6 +22,7 @@ resource "docker_network" "network_tst_docker" {
   name       = "tst-docker"
   attachable = true
   ipv6       = false
+  check_duplicate = true
   driver     = "bridge"
   ipam_config {
     ip_range = "10.30.0.0/24"
@@ -25,6 +31,10 @@ resource "docker_network" "network_tst_docker" {
   }
   options = {
     "com.docker.network.bridge.name" = "tst-docker"
+    "com.docker.network.bridge.host_binding_ipv4"    = "0.0.0.0"
+    "com.docker.network.driver.mtu"                  = "1500"
+    "com.docker.network.bridge.enable_ip_masquerade" = "true"
+    "com.docker.network.bridge.enable_icc"           = "true"
   }
 }
 
