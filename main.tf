7428ae48c038760d5330fe4e57b54acb582c079c
Kristo Aun <kristoaun@live.com>
2024-01-25 15:08:52 +0200

    host

--- main.tf
+++ main.tf
@@ -2,11 +2,23 @@ resource "docker_network" "network_dev_docker" {
   name       = "network-dev-docker"
   attachable = true
   ipv6       = false
-  driver     = "host"
+  driver     = "bridge"
   ipam_config {
     ip_range = "10.20.0.0/24"
     subnet   = "10.20.0.0/24"
     gateway  = "10.20.0.1"
+  }
+}
+
+resource "docker_network" "network_tst_docker" {
+  name       = "network-tst-docker"
+  attachable = true
+  ipv6       = false
+  driver     = "host"
+  ipam_config {
+    ip_range = "10.30.0.0/24"
+    subnet   = "10.30.0.0/24"
+    gateway  = "10.30.0.1"
   }
 }
 
