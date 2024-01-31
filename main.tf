d99326259dbf6778207074093b0b6face591a0c8
Kristo Aun <kristoaun@live.com>
2024-01-31 10:16:37 +0200

    adm

--- main.tf
+++ main.tf
@@ -1,3 +1,23 @@
+resource "docker_network" "network_adm_docker" {
+  name            = "adm-docker"
+  attachable      = true
+  ipv6            = false
+  check_duplicate = true
+  driver          = "bridge"
+  ipam_config {
+    ip_range = "10.10.0.0/24"
+    subnet   = "10.10.0.0/24"
+    gateway  = "10.10.0.1"
+  }
+  options = {
+    "com.docker.network.bridge.name"                 = "adm-docker"
+    "com.docker.network.bridge.host_binding_ipv4"    = "0.0.0.0"
+    "com.docker.network.driver.mtu"                  = "1500"
+    "com.docker.network.bridge.enable_ip_masquerade" = "true"
+    "com.docker.network.bridge.enable_icc"           = "true"
+  }
+}
+
 resource "docker_network" "network_dev_docker" {
   name            = "dev-docker"
   attachable      = true
