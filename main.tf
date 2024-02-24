6a8e2d5690ebbb88fe4252a2b6384e3cf6c345d5
Kristo Aun <kristoaun@live.com>
2024-02-24 14:59:46 +0200

    snb network

--- main.tf
+++ main.tf
@@ -58,3 +58,22 @@ resource "docker_network" "network_tst_docker" {
   }
 }
 
+resource "docker_network" "network_snb_docker" {
+  name            = "snb-docker"
+  attachable      = true
+  ipv6            = false
+  check_duplicate = true
+  driver          = "bridge"
+  ipam_config {
+    ip_range = "10.90.0.0/24"
+    subnet   = "10.90.0.0/24"
+    gateway  = "10.90.0.1"
+  }
+  options = {
+    "com.docker.network.bridge.name"                 = "snb-docker"
+    "com.docker.network.bridge.host_binding_ipv4"    = "0.0.0.0"
+    "com.docker.network.driver.mtu"                  = "1500"
+    "com.docker.network.bridge.enable_ip_masquerade" = "true"
+    "com.docker.network.bridge.enable_icc"           = "true"
+  }
+}
