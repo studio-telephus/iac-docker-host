ad6b31d5f56be9e106fbd970edade8a81a65a4fb
Kristo Aun <kristoaun@live.com>
2024-02-24 15:22:29 +0200

    run

--- docker-images.tf
+++
@@ -1,8 +0,0 @@
-data "docker_registry_image" "debian_bookworm" {
-  name = "debian:bookworm"
-}
-
-resource "docker_image" "debian_bookworm" {
-  name          = data.docker_registry_image.debian_bookworm.name
-  pull_triggers = [data.docker_registry_image.debian_bookworm.sha256_digest]
-}
